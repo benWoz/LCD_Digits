9517ae20d65cd3b258d5f7b08b3c0f7fbc586f56
Ben Woznicki <b@benWoz.com>
2015-08-16 16:15:23 -0400

    failing test for 2 digits

--- spec/lcd_digits_spec.rb
+++ spec/lcd_digits_spec.rb
@@ -5,7 +5,7 @@ class LCD_Digits
 			0 => ".-.\n" +
 			     "|.|\n" +
 			     "|_|\n",
-			     
+
 			1 => "...\n" +
 			     "..|\n" + 
 			     "..|\n"
@@ -14,6 +14,9 @@ class LCD_Digits
 
 	def number(digit)
 		@numbers[digit]
+	end
+
+	def row(digits)
 	end
 
 end
@@ -31,6 +34,14 @@ RSpec.describe LCD_Digits do
 	  it "takes '1' as a number" do
 	  	expect(lcd_digit.number(1)).to eq "...\n..|\n..|\n"
 	  end
+	end
+
+	describe "multiple digits in a row" do
+		it "represents 2 digits" do
+			expect(lcd_digit.row(01)).to eq ".-." + ' ' + "...\n"\
+																		  "|.|" + ' ' + "..|\n"\
+																		  "|_|" + ' ' + "..|\n"
+		end
 	end
 
 end
